de91440d17b6fe172d197e1843921359888ff6a3
Tristochi <TristanLSzakacs@gmail.com>
2019-05-10 10:38:06 -0400

    All mechanics of program are now working. Refactored into appropriate functions.

--- CourtesyPayLetters/CourtesyPayLetters.py
+++ CourtesyPayLetters/CourtesyPayLetters.py
@@ -1,56 +1,107 @@
-from __future__ import print_function
-from datetime import date
 from mailmerge import MailMerge
+import time
 import os   
 
-text_file = open('J:\cpletters.txt', 'r')
-courtesy_pay_list = text_file.readlines()
-text_file.close()
-delimeted_text = [line.split('\t') for line in courtesy_pay_list]
-header = delimeted_text[0]
-fifteen_days_late = []
-fifteen_days_late.append(header)
-twentyfive_days_late = []
-twentyfive_days_late.append(header)
-thirtyfive_days_late = []
-thirtyfive_days_late.append(header)
-for row in delimeted_text[1:]:
-    days_delinquent = int(row[5])
-    if days_delinquent == 15:
-        fifteen_days_late.append(row)
-    elif days_delinquent == 25:
-        twentyfive_days_late.append(row)
-    elif days_delinquent == 35:
-        thirtyfive_days_late.append(row)
-
-out15 = open('J:\cpletters15.txt', 'w+')
-out25 = open('J:\cpletters25.txt', 'w+')
-out35 = open('J:\cpletters35.txt', 'w+')
+# This method uses that delimited list and writes it to a new text file.
+def writeNewTxt(aList, output):
+    out = open(output, 'w+')
+    for line in aList:
+        out.write('\t'.join(line))
+    out.close()
 
 
-for line in fifteen_days_late:
-    out15.write('\t'.join(line))
-    out15.write('\n')
-out15.close()
+# This method takes a file path as input and a file path to write to as output.
+def sort_txt_file(input):
+    text_file = open(input, 'r')
+    newList = text_file.readlines()
+    text_file.close()
+    delimeted = [line.split('\t') for line in newList]
+    header = delimeted[0]
+    list1 = []
+    list1.append(header)
+    list2 = []
+    list2.append(header)
+    list3 = []
+    list3.append(header)
 
-for line in twentyfive_days_late:
-    out25.write('\t'.join(line))
-    out25.write('\n')
-   
-out25.close()
+    for row in delimeted[1:]:
+        days_delinquent = int(row[5])
+        if days_delinquent == 15:
+           list1.append(row)
+        elif days_delinquent == 25:
+            list2.append(row)
+        elif days_delinquent == 35:
+            list3.append(row)
 
-for line in thirtyfive_days_late:
-    out35.write('\t'.join(line))
-    out35.write('\n')
-out35.close()
+    writeNewTxt(list1, 'G:\exports\cpletters\cpletters15.txt')
+    writeNewTxt(list2, 'G:\exports\cpletters\cpletters25.txt')
+    writeNewTxt(list3, 'G:\exports\cpletters\cpletters35.txt')
 
-template = 'J:\courtesy_pay_15_test.docx'
-document = MailMerge(template)
+    count1 = len(list1) - 1
+    count2 = len(list2) - 1
+    count3 = len(list3) - 1
+    print("There are ", count1, "records on courtestpay15.txt")
+    print("There are ", count2, "records on courtestpay25.txt")
+    print("There are ", count3, "records on courtestpay35.txt")
 
-cpayltr15 = {
-    'MEMBER_NBR': 
-    }
+#This requires a docx for templatePath and outputPath. Both need to have merge fields already inplace.
+#The txtFilePath takes the path of one previously sorted txt file.
+def writeToDocx(txtFilePath, templatePath, outputPath):
+    text_file = open(txtFilePath, 'r')
+    newList = text_file.readlines()
+    text_file.close()
+    delimeted = [line.split('\t') for line in newList]
+    header = delimeted[0]
+    values = delimeted[1:]
+    template = templatePath
+    document = MailMerge(template)
+
+    big_Dict = ([{head:val for head, val in zip(header, val)} for val in values])
+    document.merge_templates(big_Dict, 'nextPage_section')
+    document.write(outputPath)
+
+def getRecordsCount(txtFilePath):
+    text_file = open(txtFilePath, 'r')
+    newList = text_file.readlines()
+    text_file.close()
+    delimeted = [line.split('\t') for line in newList]
+    header = delimeted[0]
+    values = delimeted[1:]
+
+    return int(len(values))
 
 
+sort_txt_file('G:\exports\cpletters\cpletters.txt')
 
+writeToDocx('G:\exports\cpletters\cpletters15.txt', 'G:\exports\cpletters\courtesy_pay_15_temp.docx', 
+            'G:\exports\cpletters\courtesy_pay_15.docx')
 
+writeToDocx('G:\exports\cpletters\cpletters25.txt', 'G:\exports\cpletters\courtesy_pay_25_temp.docx', 
+            'G:\exports\cpletters\courtesy_pay_25.docx')
+
+writeToDocx('G:\exports\cpletters\cpletters35.txt', 'G:\exports\cpletters\courtesy_pay_35_temp.docx', 
+            'G:\exports\cpletters\courtesy_pay_35.docx')
+
+count1 = getRecordsCount('G:\exports\cpletters\cpletters15.txt') - 1
+if count1 > 0:
+    os.startfile('G:\\exports\cpletters\courtesy_pay_15.docx', 'print')
+    time.sleep(2)
+    print(count1, " 15 day records printed.")
+else:
+    print("No 15 day records to print.")
+
+count2 = getRecordsCount('G:\exports\cpletters\cpletters25.txt') - 1
+if count2 > 0:
+    os.startfile('G:\\exports\cpletters\courtesy_pay_25.docx', 'print')
+    time.sleep(2)
+    print(count2, " 25 day records printed.")
+else:
+    print("No 25 day records to print.")
+
+count3 = getRecordsCount('G:\exports\cpletters\cpletters35.txt') - 1
+if count3 > 0:
+    os.startfile('G:\\exports\cpletters\courtesy_pay_35.docx', 'print')
+    time.sleep(2)
+    print(count3, " 35 day records printed.")
+else:
+    print("No 35 day records to print.")
